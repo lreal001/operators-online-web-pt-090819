01301f7098a41134feb84ce56fb81106b582cc0a
Lessllie Real <rlessllie@yahoo.com>
2019-09-21 03:05:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,29 @@
 def unsafe?(speed)
-
+if speed < 40  
+  return true
+end
+if speed > 60
+  return true
+else
+  return false
+end
 end
 
 
 
 def not_safe?(speed)
-	
-end
+ speed = 96
+ if speed > 60  
+   return true
+ end
+ if speed < 40 
+   return true
+ end 
+ speed (40..60) ? false: true 
+ 
+ end
+  
+  
 	
 
 
